226db1959d13d82872c74f0193b39e8e8ca82c27
Jake <test@fake.com.au>
2025-08-10 20:48:59 -0500

    working out sprite sheets

--- scenes/WalkingScene.js
+++ scenes/WalkingScene.js
@@ -14,11 +14,37 @@ export default class WalkingScene extends Phaser.Scene {
     preload() {
         this.load.image('bg_dcr', 'assets/UI/bg_route_daycareroad.png');
         this.load.image('bg_textbox', 'assets/UI/bg_walking_textbox.png');
+        this.load.image('hoenn', 'assets/spritesheets/hoenn_trans.png');
     }
 
     create() {
         this.add.image(0, 0, 'bg_dcr').setOrigin(0, 0);
         this.add.image(0, 700, 'bg_textbox').setOrigin(0, 0);
+
+        //uhh this is some code im stealing 
+        player = this.load.spritesheet(100, 450, 'hoenn');
+        
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('hoenn', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'hoenn', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('hoenn', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
     }
 
     update(time, delta) {
